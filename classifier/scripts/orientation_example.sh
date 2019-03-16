834f7880f1bed336426d448eebaaec1bd33d4366
mrtimmer <tim.wengefeld@tu-ilmenau.de>
2019-03-16 08:18:11 +0100

    changed package name in example script

--- classifier/scripts/orientation_example.sh
+++ classifier/scripts/orientation_example.sh
@@ -1,2 +1,2 @@
-rosrun pointcloud_orientation_estimation orientation_estimation_example _model:=`rospack find pointcloud_orientation_estimation`/learned_models/precise/top_down_multiclass_classifier.yaml _exampleFilesPath:=`rospack find pointcloud_orientation_estimation`/data/orientation_examples
+rosrun pointcloud_person_orientation_estimation orientation_estimation_example _model:=`rospack find pointcloud_person_orientation_estimation`/learned_models/precise/top_down_multiclass_classifier.yaml _exampleFilesPath:=`rospack find pointcloud_person_orientation_estimation`/data/orientation_examples
 
